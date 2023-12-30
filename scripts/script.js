7a599b73e1a4d7ea9bdb93ad93b5f28dd88d02de
LlayN <h.yengo@outlook.fr>
2023-12-30 23:13:15 +0100

    Mise en place du header + script JS pour menu burger

--- scripts/script.js
+++ scripts/script.js
@@ -1,1 +1,12 @@
-let test = 2;
+let menuIcon = document.querySelector('.menuIcon')
+let menuList = document.querySelector('.menuList')
+
+menuIcon.addEventListener('click', () => {
+   
+  if(menuList.style.display === ""){
+    menuList.style.display = 'block';
+  }
+  else{
+    menuList.style.display = '';
+  }
+})
