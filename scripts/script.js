1dcc9668b60b3c35611bc79b0bb717434085c1d8
LlayN <95718887+LlayN@users.noreply.github.com>
2024-01-02 03:26:11 +0100

    MAJ Navigation, MAJ pages HTML

--- scripts/script.js
+++ scripts/script.js
@@ -1,12 +1,23 @@
-let menuIcon = document.querySelector('.menuIcon')
-let menuList = document.querySelector('.menuList')
+const menuIcon = document.querySelector('.menuIcon')
+const menuList = document.querySelector('.menuList')
+const middle = document.querySelector('.middle')
 
 menuIcon.addEventListener('click', () => {
-   
+    
   if(menuList.style.display === ""){
     menuList.style.display = 'block';
+    
   }
   else{
     menuList.style.display = '';
+  }
+
+  if(!menuList.classList.contains('btn_active')){
+    menuList.classList.add('btn_active')
+    middle.classList.add('btn_active')
+  }
+  else{
+    menuList.classList.remove('btn_active')
+    middle.classList.remove('btn_active')
   }
 })
